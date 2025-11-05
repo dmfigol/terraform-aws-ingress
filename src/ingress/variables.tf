3fe08dba41c49e06df09c77adcba1588c9c897cb
Dmitry Figol <git@dmfigol.me>
2025-11-05 22:37:54 +0100

    Update security group interface

--- src/ingress/variables.tf
+++ src/ingress/variables.tf
@@ -64,21 +64,21 @@ variable "certificates" {
 }
 
 variable "security_groups" {
-  description = "Map of security groups to create"
   type = map(object({
-    description = string
-    inbound = list(object({
-      protocol    = string
-      ports       = string
-      source      = string
-      description = string
-    }))
-    outbound = list(object({
-      protocol    = string
-      ports       = string
-      source      = string
-      description = string
-    }))
+    description = optional(string, "")
+    inbound = optional(list(object({
+      protocol    = optional(string, "-1")
+      ports       = optional(string, null) # Format: "443,8080-8081,9000". When null and protocol is -1, means all ports.
+      source      = optional(string, null) # Format: "10.0.0.0/8,192.168.1.0/24,2001:db8::/32" or "sg-name"
+      description = optional(string, "")
+    })), [])
+    outbound = optional(list(object({
+      protocol    = optional(string, "-1")
+      ports       = optional(string, null) # Format: "443,8080-8081,9000". When null and protocol is -1, means all ports.
+      destination = optional(string, null) # Format: "10.0.0.0/8,192.168.1.0/24,2001:db8::/32" or "sg-name"
+      description = optional(string, "")
+    })), [])
+    tags = optional(map(string), {})
   }))
   default = {}
 }
